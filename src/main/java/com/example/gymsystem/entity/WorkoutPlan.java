6a240acfe723f638f91ad6bd1098c39f1cd17d52
Buruleanu <buruleanudaniel@gmail.com>
2025-02-06 12:49:11 +0200

    fixed WorkoutPlan getters and setters

--- src/main/java/com/example/gymsystem/entity/WorkoutPlan.java
+++ src/main/java/com/example/gymsystem/entity/WorkoutPlan.java
@@ -17,7 +17,7 @@ public class WorkoutPlan {
     private LocalDate startDate;
 
     @ManyToOne
-    @JoinColumn(name = "member_id") // coloana de foreign key
+    @JoinColumn(name = "member_id") // Foreign key column
     private Member member;
 
     @ManyToOne
@@ -37,38 +37,6 @@ public class WorkoutPlan {
         this.description = description;
         this.startDate = LocalDate.now();
         this.member = member;
-        this.trainer = trainer;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
-    }
-
-    public Trainer getTrainer() {
-        return trainer;
-    }
-
-    public void setTrainer(Trainer trainer) {
         this.trainer = trainer;
     }
 }
